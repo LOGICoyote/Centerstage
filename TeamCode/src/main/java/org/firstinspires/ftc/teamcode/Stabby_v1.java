8cc85628d0d707b35a76e6138e3bc9b8a326e238
Lydia754 <woodall.lydia@gmail.com>
2023-12-08 12:57:51 -0500

    final pre-comp fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Stabby_v1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Stabby_v1.java
@@ -61,13 +61,29 @@ public class Stabby_v1 extends OpMode {
     private Servo Lslideshift;
     private DcMotor Llift;
     private DcMotor Rlift;
-    private DcMotor Llifttest;
+    private Servo lclawturn;
+    private Servo rclawturn;
+    private Servo rclaw;
+    private Servo lclaw;
 
-    private double slideoutconstant = 0.8;
+    //private DcMotor Llifttest;
+
+    private double slideoutconstant = 0.3;
     private PIDRunner rSlidePID;
     private PIDRunner lSlidePID;
     private double precisemovespeed=0.5;
     private double desSlideHeight = 0.0;
+    private boolean ypressed = false;
+    private boolean clawclosed = true;
+    private boolean dpaduppressed = false;
+    private boolean dpaddownpressed = false;
+    private boolean twodlpressed = false;
+    private boolean twodrpressed = false;
+    private boolean lclawclosed = true;
+    private boolean rclawclosed = true;
+
+
+
 
     @Override
     public void init() {
@@ -83,20 +99,25 @@ public class Stabby_v1 extends OpMode {
         intake  = hardwareMap.get(DcMotor.class, "intake");
         Lslideshift=hardwareMap.get(Servo.class, "Lshift");
         Rslideshift=hardwareMap.get(Servo.class, "Rshift");
-       // Llift = hardwareMap.get(DcMotor.class, "llift");
-        Llifttest=hardwareMap.get(DcMotor.class, "xlift");
+        Llift = hardwareMap.get(DcMotor.class, "llift");
         Rlift = hardwareMap.get(DcMotor.class, "rlift");
+        lclawturn=hardwareMap.get(Servo.class, "lclawturn");
+        rclawturn=hardwareMap.get(Servo.class, "rclawturn");
+        lclaw=hardwareMap.get(Servo.class, "lclaw");
+        rclaw=hardwareMap.get(Servo.class, "rclaw");
 
-        rSlidePID = new PIDRunner(0.03, 0, 0, getRuntime());
-        lSlidePID = new PIDRunner(0.03, 0, 0, getRuntime());
+        rSlidePID = new PIDRunner(0.1, 0, 0, getRuntime());
+        lSlidePID = new PIDRunner(0.1, 0, 0, getRuntime());
         
 
         leftFront.setDirection(DcMotor.Direction.REVERSE);
-        rightFront.setDirection(DcMotor.Direction.FORWARD);
-        leftBack.setDirection(DcMotor.Direction.FORWARD);
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        leftBack.setDirection(DcMotor.Direction.REVERSE);
         rightBack.setDirection(DcMotor.Direction.FORWARD);
-        intake.setDirection(DcMotor.Direction.REVERSE);
-        Rlift.setDirection(DcMotorSimple.Direction.REVERSE);
+       // intake.setDirection(DcMotor.Direction.REVERSE);
+//        Rlift.setDirection(DcMotorSimple.Direction.REVERSE);
+//        Llift.setDirection(DcMotorSimple.Direction.REVERSE);
+
 
         leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -110,8 +131,10 @@ public class Stabby_v1 extends OpMode {
     @Override
     public void start() {
         runtime.reset();
-        //Llift.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        Llift.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         Rlift.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        Llift.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        Rlift.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
     }
 
     @Override
@@ -131,38 +154,38 @@ public class Stabby_v1 extends OpMode {
         left_trigger = gamepad1.left_trigger;
         right_trigger = gamepad1.right_trigger;
 
-//        if (left_trigger > 0) {
-//            turnleft(left_trigger);
-//
-//        } else if (right_trigger > 0) {
-//            turnright(right_trigger);
-//
-//        } else if (right_XAxis > 0.2) {
-//            straferight(right_XAxis * precisemovespeed);
-//
-//        } else if (right_XAxis < -0.2) {
-//            strafeleft(right_XAxis * -precisemovespeed);
-//
-//        } else if (right_YAxis > 0.2) {
-//            goforward(right_YAxis * precisemovespeed);
-//
-//        } else if (right_YAxis < -0.2) {
-//            gobackward(right_YAxis * precisemovespeed);
-//
-//        } else if (left_XAxis > 0.4) {
-//            straferight(left_XAxis);
-//
-//        } else if (left_XAxis < -0.4) {
-//            strafeleft(-left_XAxis);
-//
-//        } else if (left_YAxis > 0.2) {
-//            goforward(left_YAxis);
-//
-//        } else if (left_YAxis < -0.2) {
-//            gobackward(left_YAxis);
-//
-//        } else
-//            gostop(0);
+        if (left_trigger > 0) {
+            turnleft(left_trigger);
+
+        } else if (right_trigger > 0) {
+            turnright(right_trigger);
+
+        } else if (right_XAxis > 0.2) {
+            straferight(right_XAxis * precisemovespeed);
+
+        } else if (right_XAxis < -0.2) {
+            strafeleft(right_XAxis * -precisemovespeed);
+
+        } else if (right_YAxis > 0.2) {
+            goforward(right_YAxis * precisemovespeed);
+
+        } else if (right_YAxis < -0.2) {
+            gobackward(right_YAxis * precisemovespeed);
+
+        } else if (left_XAxis > 0.4) {
+            straferight(left_XAxis);
+
+        } else if (left_XAxis < -0.4) {
+            strafeleft(-left_XAxis);
+
+        } else if (left_YAxis > 0.2) {
+            goforward(left_YAxis);
+
+        } else if (left_YAxis < -0.2) {
+            gobackward(left_YAxis);
+
+        } else
+            gostop(0);
 
         //intake controls
         if (gamepad1.right_bumper) {
@@ -176,26 +199,138 @@ public class Stabby_v1 extends OpMode {
         }
         if (gamepad1.dpad_left){
             Lslideshift.setPosition(0);
-            Rslideshift.setPosition(0);
-
+            Rslideshift.setPosition(1);
         }
         if (gamepad1.dpad_right){
+            Lslideshift.setPosition(.15);
+            Rslideshift.setPosition(.85);
             Lslideshift.setPosition(slideoutconstant);
-            Rslideshift.setPosition(slideoutconstant);
+            Rslideshift.setPosition(1-slideoutconstant);
         }
-
+        if (gamepad1.dpad_down){
+            desSlideHeight = convertToCM(0);
+        }
         // lift controls
         if (gamepad1.a) {
-            desSlideHeight = convertToCM(3.0);
+            desSlideHeight = convertToCM(10.0);
         }
         if (gamepad1.b) {
-            desSlideHeight = convertToCM(6.0);
+            desSlideHeight = convertToCM(25);
         }
-       // lift(desSlideHeight); // run this every cycle
-        if (gamepad1.x){
-            Llifttest.setPower(.5);
+//        if (gamepad1.y) {
+//            //closed
+//            lclaw.setPosition(0.25);
+//           rclaw.setPosition(.75);
+//        }
+//        if (gamepad1.x) {
+//            //open
+//            lclaw.setPosition(.4);
+//            rclaw.setPosition(.6);
+//        }
+
+        if (gamepad1.y) {
+            if (!ypressed) {
+                if (clawclosed){
+                    lclaw.setPosition(.4);
+                    rclaw.setPosition(.6);
+                    clawclosed = false;
+                    lclawclosed=false;
+                    rclawclosed= false;
+                }
+                else {
+                    lclaw.setPosition(0.25);
+                    rclaw.setPosition(.75);
+                    clawclosed= true;
+                    lclawclosed = true;
+                    rclawclosed = true;
+                }
+            }
+            ypressed=true;
+        } else
+            ypressed=false;
+
+        //driver 2
+        if (gamepad2.left_bumper) {
+            if (!twodlpressed) {
+                if (lclawclosed){
+                    lclaw.setPosition(.4);
+                    lclawclosed = false;
+                }
+                else {
+                    lclaw.setPosition(0.25);
+                    lclawclosed= true;
+                }
+            }
+            twodlpressed=true;
+        } else
+            twodlpressed = false;
+
+        if (gamepad2.right_bumper) {
+            if (!twodrpressed) {
+                if (rclawclosed){
+                    rclaw.setPosition(.6);
+                    rclawclosed = false;
+                }
+                else {
+                    rclaw.setPosition(0.75);
+                    rclawclosed= true;
+                }
+            }
+            twodrpressed=true;
+        } else
+            twodrpressed = false;
+//        if (gamepad2.left_bumper) {
+//            lclaw.setPosition(.4);
+////        }
+//        if (gamepad2.right_bumper) {
+//            rclaw.setPosition(.6);
+//        }
+
+        if (gamepad2.a) {
+            lclawturn.setPosition(.25);
+            rclawturn.setPosition(.75);
+        }
+        if (gamepad2.b) {
+            lclawturn.setPosition(0.5);
+            rclawturn.setPosition(0.5);
+        }
+        if (gamepad2.y) {
+            //up tilt
+            lclawturn.setPosition(.9);
+            rclawturn.setPosition(0.1);
+        }
+        if (gamepad2.dpad_right){
+            Lslideshift.setPosition(.15);
+            Rslideshift.setPosition(.85);
+            Lslideshift.setPosition(slideoutconstant);
+            Rslideshift.setPosition(1-slideoutconstant);
         }
 
+        if (gamepad2.dpad_up) {
+            if (!dpaduppressed) {
+                desSlideHeight += convertToCM(1.0);
+            }
+            dpaduppressed=true;
+        } else
+            dpaduppressed=false;
+        if (gamepad2.dpad_down) {
+            if (!dpaddownpressed) {
+                desSlideHeight -= convertToCM(1.0);
+            }
+            dpaddownpressed=true;
+        } else
+            dpaddownpressed=false;
+//        if (gamepad2.dpad_up){
+//            desSlideHeight += convertToCM(1.0);
+//        }
+//        if (gamepad2.dpad_down){
+//            desSlideHeight -= convertToCM(1.0);
+//        }
+        if (gamepad2.dpad_left){
+            Lslideshift.setPosition(0);
+            Rslideshift.setPosition(1);
+        }
+        lift(desSlideHeight); // run this every cycle
     }
 
     @Override
@@ -209,7 +344,7 @@ public class Stabby_v1 extends OpMode {
 
     private void lift(double liftpos) {
 
-        double rPosition = Rlift.getCurrentPosition();
+        double rPosition = -Rlift.getCurrentPosition();
         double lPosition = Llift.getCurrentPosition();
         
         double rHeightCM = (3.18 * rPosition * Math.PI) / 145.1;
@@ -238,8 +373,8 @@ public class Stabby_v1 extends OpMode {
         // }
         // Llift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         // uncomment when ready to test
-        Llift.setPower(-0.5);
-        Rlift.setPower(-0.5);
+        Llift.setPower(lPower);
+        Rlift.setPower(-rPower);
         telemetry.addData("lift r pos", rHeightCM);
         telemetry.addData("lift l pos", lHeightCM);
         telemetry.addData("despos", liftpos);
@@ -252,7 +387,7 @@ public class Stabby_v1 extends OpMode {
         turnspeed = 0.5;
     }
     private void intakeforward() {
-        intake.setPower(.7);
+        intake.setPower(.9);
     }
     private void intakeoff() {
         intake.setPower(0);
